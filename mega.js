45e848771cd83cf3be905b9947ca633ec1625dea
Anuhas20091119 <yasiruanuhas579@gmail.com>
2026-02-24 01:24:25 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'dchuti070@gmail.com',
+    password: 'Anuhas20',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
